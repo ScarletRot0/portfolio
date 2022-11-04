b047f821a62299ea48f387f5916695daf10e9fb7
ScarletRot0 <jlobo2301@gmail.com>
2022-11-03 22:29:41 -0500

    borrar

--- ejercicioenclase.php
+++
@@ -1,8 +0,0 @@
-<?php
-$stock=800;
-if($stock>0){
-	echo "Hay stock";
-}
-else{
-    echo "NO hay stock";
-}
--- rand.php
+++
@@ -1,3 +0,0 @@
-// Un =, es el operador de asignacion
-// Doble ==, es el operador de comparacion por contenido
-// Triplpe ===, es el operador de comparacion por tipo de variable y por contenido
